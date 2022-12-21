5c714b50f7d807da79dd9c32cbe59bf9ccd93278
Rico Huijbers <rix0rrr@gmail.com>
2022-12-21 13:16:13 +0100

    Better TS tests

--- packages/@aws-cdk-testing/cli-integ/tests/init-typescript-lib/init-typescript-lib.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/init-typescript-lib/init-typescript-lib.integtest.ts
@@ -1,22 +1,13 @@
 import { integTest, withTemporaryDirectory, ShellHelper, withPackages } from '../../lib';
-import { typescriptVersionsSync } from '../../lib/npm';
 
-typescriptVersionsSync().forEach(tsVersion => {
-  integTest(`typescript ${tsVersion} init lib`, withTemporaryDirectory(withPackages(async (context) => {
-    const shell = ShellHelper.fromContext(context);
-    await context.packages.makeCliAvailable();
+integTest('typescript init lib', withTemporaryDirectory(withPackages(async (context) => {
+  const shell = ShellHelper.fromContext(context);
+  await context.packages.makeCliAvailable();
 
-    await shell.shell(['node', '--version']);
-    await shell.shell(['npm', '--version']);
+  await shell.shell(['cdk', 'init', '-l', 'typescript', 'lib']);
 
-    await shell.shell(['cdk', 'init', '-l', 'typescript', 'lib', '--generate-only']);
-    await shell.shell(['npm', 'install', '--save-dev', `typescript@${tsVersion}`]);
-    await shell.shell(['npm', 'install']); // Older versions of npm require this to be a separate step from the one above
-    await shell.shell(['npx', 'tsc', '--version']);
-
-    await shell.shell(['npm', 'prune']);
-    await shell.shell(['npm', 'ls']); // this will fail if we have unmet peer dependencies
-    await shell.shell(['npm', 'run', 'build']);
-    await shell.shell(['npm', 'run', 'test']);
-  })));
-});
+  await shell.shell(['npm', 'prune']);
+  await shell.shell(['npm', 'ls']); // this will fail if we have unmet peer dependencies
+  await shell.shell(['npm', 'run', 'build']);
+  await shell.shell(['npm', 'run', 'test']);
+})));
