20eec6a7af329c46f9dd95fc448d0177d0de6739
Rico Huijbers <rix0rrr@gmail.com>
2023-01-03 16:14:31 +0100

    REVERTME: Fake regression version to test scripts

--- packages/aws-cdk/test/integ/test-cli-regression-against-current-code.sh
+++ packages/aws-cdk/test/integ/test-cli-regression-against-current-code.sh
@@ -9,4 +9,4 @@ previous=$(${INTEG_TOOLS}/bin/query-github last-release --token $GITHUB_TOKEN --
 echo "Previous version is: $previous"
 
 # Old tests, new CLI, new framework
-exec $INTEG_TOOLS/bin/download-and-run-old-tests "$previous" --use-version=$VERSION cli-integ-tests
+exec $INTEG_TOOLS/bin/download-and-run-old-tests "$VERSION" --use-version=$VERSION cli-integ-tests
